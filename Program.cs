36433385bb9448f94b0d50caa5f29f4996eb25eb
xejzi <vadimdoromanov2006@gmail.com>
2024-09-30 11:12:09 +0900

    Программа запрашивает имя пользователя

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System; 
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите ваше имя: ");
+        string name = Console.ReadLine(); 
+        Console.WriteLine($"Hello world from {name}!");
+    }
+}
